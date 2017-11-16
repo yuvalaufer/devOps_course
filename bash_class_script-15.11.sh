416e30fcfbdef6b96717e7eba2e39516c78241bf
Yuval Laufer <yuvalaufer@yahoo.com>
2017-11-16 12:41:31 +0200

    updating 15.11 script

--- bash_class_script-15.11.sh
+++ bash_class_script-15.11.sh
@@ -9,7 +9,7 @@ echo "today's date is: $(date +"%d/%m/%Y")"
 echo "your age is: $age1"
 
 if [ `who -b |awk '{print $3}'` == `date +"%Y-%m-%d"` ]; then
-	sys_up=`uptime |awk '{print $3,$4}'`
+	sys_up=`uptime |awk '{print $3,$4}' |awk -F, '{print $1}'`
 else
 	sys_up=`uptime |awk '{print $3,$4,$5,$6}'`
 fi
