62c6e28dfda2cdf4d2405c0371a923850fdca835
Yuval Laufer <yuvalaufer@yahoo.com>
2017-11-19 00:55:46 +0200

    update elif in 15.11 script

--- bash_class_script-15.11.sh
+++ bash_class_script-15.11.sh
@@ -8,10 +8,12 @@ echo "there are: $(ls -lrt ~ |grep -v total |wc -l) files in your home directory
 echo "today's date is: $(date +"%d/%m/%Y")"
 echo "your age is: $age1"
 
-if [ `/usr/bin/uptime |awk '{print $4}' |awk -F, '{print $1}'` != "days" ]; then
-	sys_up=`/usr/bin/uptime |awk '{print $3,$4}' |awk -F, '{print $1}'`
-else
+if [ `/usr/bin/uptime |awk '{print $4}' |awk -F, '{print $1}'` == "days" ]; then
 	sys_up=`/usr/bin/uptime |awk '{print $3,$4,$5,$6}'`
+elif [ `/usr/bin/uptime |awk '{print $4}' |awk -F, '{print $1}'` == "day" ]; then
+	sys_up=`/usr/bin/uptime |awk '{print $3,$4,$5}' |awk -F, '{print $1,",",$2}'`
+else
+	sys_up=`/usr/bin/uptime |awk '{print $3,$4}' |awk -F, '{print $1}'`
 fi
 
 echo "the system is up for: $sys_up"
