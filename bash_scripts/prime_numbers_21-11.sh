8eeedd271dd32b1e5be1411729eaa8aafc9c8457
yuvalaufer <yuvalaufer@yahoo.com>
2017-11-21 23:47:31 +0200

    initial prime_numbers script

---
+++ bash_scripts/prime_numbers_21-11.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -x
+num1=$1
+if [ -z $num1 ]; then
+	echo "enter a number to check if it is a prime number"
+	read num1
+fi
+
+for i in "2" "3" "5" "7"; do
+	if [ "$num1" == "$i" ] ;then
+		echo "$num1 is a prime number"
+		exit 0;
+
+	elif [ "$(expr $num1 % $i)" == "0" ] ;then
+		echo "$num1 is not a prime number"
+		exit 0;
+done
+
+	k=$(expr `expr $num1 - 1` / 2)
+	for ((i=3;i<$k;i++)); do
+		if [ `expr $num1 % $i` == "0" ]; then
+			echo "$num1 is not a prime number , since it devides in $i"
+			exit 0;
+		fi
+	echo "$num1 is a prime number!!!"
+	done
