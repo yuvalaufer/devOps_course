0f7c530f533664c9d7d04d31c553eb22d6ba1b51
yuvalaufer <yuvalaufer@yahoo.com>
2017-11-23 16:37:34 +0200

    updating longest-shortest user script

--- bash_scripts/longest_shortest_user.sh
+++ bash_scripts/longest_shortest_user.sh
@@ -1,13 +1,48 @@
-#!/bin/bash
-usercount=`cat /etc/passwd |awk -F: '{print }' |wc -l`
+#!/bin/bash -x
 
-for ((i=0;i<$usercount;i++)); do
-	cat /etc/passwd |awk -F: '{print $1}' |awk -v n=$i 'NR == n' |wc -L
-	
+cat /etc/passwd |awk -F: '{print $1}' > user_list
+usercount=$(cat user_list | wc -l)
+longl=$(cat user_list |wc -L)
+
+
+function longest_user {
+for ((i=1;i<=$usercount;i++)); do
+        cur_user=$(awk -v n=$i 'NR == n' user_list)
+        if [ "$(echo "$cur_user" | wc -L)" == "$longl" ]; then
+                echo "$cur_user"
+		fi
+done
+}
+
+function shortest_user () {
+sindex=0
+usernum=$1
+short_list[$sindex]=$(awk 'NR == 1' user_list)
+for ((i=2;i<=$usernum;i++)); do
+	cur_user=$(awk -v n=$i 'NR == n' user_list)
+	if [ "$(echo "$cur_user" |wc -L)" -lt "$(echo "${short_list[$sindex]}" |wc -L)" ];then 
+		short_list=()
+		sindex=0
+		short_list[$sindex]="$cur_user"
+	elif [ "$(echo "$cur_user" |wc -L)" == "$(echo "${short_list[$sindex]}" |wc -L)" ]; then
+		(( sindex++ )) 
+		short_list[$sindex]=$cur_user
+	fi
 done
 
+for ((i=0;i<=$sindex;i++)); do
+	echo "${short_list[$i]}"
+done
+#echo "${short_list[@]}"
+}
 
+echo """
+the longest username/usernames in the system:"""
+longest_user
 
-#cat /etc/passwd |awk -F: '{print }' |awk 'FNR==5'
-#cat /etc/passwd |awk -F: '{print }' |wc -l
-#test 3 -ge 6 && echo var1 is larger || echo var2 is larger 
+echo """
+the shortest username/usernames in the system:"""
+shortest_user "$usercount"
+
+#end of script
+
