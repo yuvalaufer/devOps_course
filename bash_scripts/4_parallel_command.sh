75e8a5f34d6820babbff0c630b7ccc63f5536921
Yuval.L <yuvalaufer@yahoo.com>
2017-11-30 15:48:25 +0200

    update in 4 script

--- bash_scripts/4_parallel_command.sh
+++ bash_scripts/4_parallel_command.sh
@@ -5,3 +5,11 @@ read servercount
 echo "what is the command you want to send?"
 read comm1
 
+# maybe try to do it in a function...
+for ((i=1;i<=$servercount;i++)); do
+	echo "enter ip address for server no. $i":
+	read serverip.$i
+done
+
+#"avoid the ssh check and the need to write yes in the first command"
+#"ssh -o StrictHostKeyChecking=no 192.168.137.139 -l root"
