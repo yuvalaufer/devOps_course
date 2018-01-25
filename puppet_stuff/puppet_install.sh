62ca581d681357a5dc16b5287e2fbadfc325de38
Cloud User <centos@ip-172-31-13-91.us-east-2.compute.internal>
2018-01-25 16:20:38 +0000

    updating puppet installation script

--- puppet_stuff/puppet_install.sh
+++ puppet_stuff/puppet_install.sh
@@ -3,6 +3,14 @@ install_type=$1
 
 function install_master {
 echo "running puppet master installation............"
+#sudo rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
+wait
+#sudo yum -y install puppetserver
+wait
+test "$(free -m |grep Mem |awk '{print $2}')" -le "2024" && sed -i 's/JAVA_ARGS="*".*/JAVA_ARGS="-Xms256m -Xmx256m -XX:MaxPermSize=256m"/g' /etc/sysconfig/puppetserver
+systemctl start puppetserver ; wait
+systemctl enable puppetserver
+
 }
 
 function install_client {
