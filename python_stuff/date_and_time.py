aa94d79102d536fdd32dc2ee8858b5b83a084e1b
Yuval <yuvalaufer@yahoo.com>
2018-03-04 22:23:32 +0200

    adding python stuff

---
+++ python_stuff/date_and_time.py
@@ -0,0 +1,7 @@
+#first import date and time
+from datetime import datetime 
+# assign the output of the datetime.now to now parameter
+now = datetime.now()
+
+print '%s\%s\%s' % (now.day, now.month, now.year)
+print '%s:%s:%s' % (now.hour, now.minute, now.second)
